5501ef74a11d9afb683e94cfe86e34ff38fcadd7
John McCormack <mccormack.wgsi@gmail.com>
2020-07-28 11:47:25 -0400

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1 class="mb-0">
+      JD
+      <span class="text-primary">McCormack</span>
+    </h1>
+    <div class="subheading text-secondary mb-5">
+      Boston, MA · (617) 257-5586 ·
+      <a href="mailto:me@jdmccormack.com">me@jdmccormack.com</a>
+    </div>
+    <p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level
+      overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value
+      proposition.</p>
+    <div class="social-icons">
+      <a class="social-icon" href="#">
+        <font-awesome-icon :icon="['fab', 'linkedin-in']"/>
+      </a>
+      <a class="social-icon" href="#">
+        <font-awesome-icon :icon="['fab', 'github']"/>
+      </a>
+      <a class="social-icon" href="#">
+        <font-awesome-icon :icon="['fab', 'medium']"/>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 6rem;
+  text-transform: uppercase;
+}
+
+.subheading {
+  text-transform: uppercase;
+}
+
+.social-icons {
+  display: flex;
+  width: 100%;
+  justify-content: left;
+}
+
+.social-icon {
+  display: inline-flex;
+  align-items: center;
+  height: 3.5rem;
+  width: 3.5rem;
+  justify-content: center;
+  background-color: #495057;
+  color: #fff;
+  border-radius: 100%;
+  font-size: 1.5rem;
+  margin-right: 1.5rem;
+}
+
+.social-icon:hover {
+  background-color: #ff4500
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faLinkedinIn, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faLinkedinIn, faGithub, faMedium)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
